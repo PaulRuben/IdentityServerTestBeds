de1e583271574128f453cfc7bd132de735a0eeb5
Paul Ruben <paul@rubenengineering.com>
2017-07-11 17:19:10 -0600

    Add project files.

---
+++ Api4/Startup.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Api4
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddCors(options=>
+            //{
+            //    // this defines a CORS policy called "default"
+            //    options.AddPolicy("default", policy =>
+            //    {
+            //        policy.WithOrigins("http://localhost:4003")
+            //            .AllowAnyHeader()
+            //            .AllowAnyMethod()
+            //            .WithExposedHeaders("WWW-Authenticate");
+            //    });
+            //});
+
+            //services.AddMvcCore()
+            //    .AddAuthorization()
+            //    .AddJsonFormatters();
+            services
+                .AddMvcCore()
+                .AddJsonFormatters()
+                .AddAuthorization();
+
+            services.AddWebEncoders();
+            services.AddCors();
+            services.AddDistributedMemoryCache();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // this uses the policy called "default"
+     //       app.UseCors("default");
+
+            app.UseCors(policy =>
+            {
+                policy.WithOrigins(
+                    "http://localhost:4003");
+
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.WithExposedHeaders("WWW-Authenticate");
+            });
+
+            app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            {
+                Authority = "http://localhost:4000",
+                RequireHttpsMetadata = false,
+                EnableCaching = false,
+                ApiName = "api1"
+            });
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ IdentityServer4Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace IdentityServer4Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:4000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+
+        }
+    }
+}
